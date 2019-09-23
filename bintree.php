9b0e9879b08ba347dae581cc4ebf544289d4a927
Mr-XYS <xys159753@gmail.com>
2019-09-23 22:26:02 +0800

    新增 editorconfig 文件

--- bintree.php
+++ bintree.php
@@ -227,3 +227,18 @@ class AVLTree
         }
     }
 }
+
+
+$avlTree = new AVLTree();
+$avlTree->insert(3);
+$avlTree->insert(2);
+$avlTree->insert(1);
+$avlTree->insert(4);
+$avlTree->insert(5);
+$avlTree->insert(6);
+$avlTree->insert(7);
+$avlTree->insert(10);
+$avlTree->insert(9);
+$avlTree->insert(8);
+midOrderTraverse($avlTree->getTree());  // 中序遍历生成的二叉树看是否是二叉排序树
+print_r($avlTree->getTree());           // 以数组形式打印构建的二叉树看是否是平衡二叉树
