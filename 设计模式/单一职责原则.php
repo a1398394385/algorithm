fcadc1d30381614cae377de0902781a9b13c0b11
Mr.XYS <xys159753@gmail.com>
2019-09-30 17:58:03 +0800

    feat(test): 新增单一职责原则案例

---
+++ 设计模式/单一职责原则.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\qrcode\model;
+
+class Factory
+{
+	private $machine = '机器Z';
+
+	public function processX($material)
+	{
+		$machine = $this->machine;
+		echo "使用 {$machine} 将 {$material} 加工成产品 X \n";
+	}
+
+	public function processY($material)
+	{
+		$machine = $this->machine;
+		echo "使用 {$machine} 将 {$material} 加工成产品 Y \n";
+	}
+}
+
+$factory = new Factory();
+$factory->processX('原料1');
+$factory->processY('原料2');
