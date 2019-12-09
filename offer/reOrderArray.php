940aaf7debc2656a7d35079d271c219a2d443592
Mr.XYS <xys159753@gmail.com>
2019-12-09 22:55:05 +0800

    调整数组顺序使奇数位于偶数前面

--- offer/reOrderArray.php
+++ offer/reOrderArray.php
@@ -20,3 +20,43 @@ function reOrderArray($array)
 
     return $array;
 }
+
+
+// TODO: Wrong way
+// function reOrderArray1(array $array)
+// {
+//     $lenght = count($array);
+//     for ($odd = 0; $odd < $lenght; $odd++) {
+//         if (($array[$odd] & 1) == 0) {
+//             for ($even = $odd + 1; $even < $lenght; $even++) {
+//                 if (($array[$even] & 1) == 1) {
+//                     $temp = $array[$odd];
+//                     $array[$odd] = $array[$even];
+//                     $array[$even] = $temp;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     return $array;
+// }
+
+function reOrderArray2(array $array)
+{
+    $lenght = count($array) - 1;
+    for ($i = 0; $i < $lenght; $i++) {
+        for ($j = 0; $j < $lenght - $i; $j++) {
+            if (!($array[$j] & 1) && ($array[$j + 1] & 1)) {
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $array;
+}
+
+$arr = [1, 2, 3, 4, 5, 6, 7];
+// var_dump(reOrderArray1($arr));
