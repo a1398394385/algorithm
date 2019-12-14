387db2640e6b0219d3d0d12862d37824ce555291
Mr.XYS <xys159753@gmail.com>
2019-12-14 17:54:44 +0800

    二叉树中和为某一值的路径

--- offer/findPath.php
+++ offer/findPath.php
@@ -14,7 +14,7 @@ class TreeNode
 }
 
 /**
- * Undocumented function
+ * 二叉树中和为某一值的路径
  *
  * @param TreeNode $root
  * @param int $expectNumber
@@ -47,19 +47,3 @@ usort($arr, function ($a, $b) {
     return count($b) <=> count($a);
 });
 dd($arr);
-
-// public class Solution {
-//     private ArrayList<ArrayList<Integer>> listAll = new ArrayList<ArrayList<Integer>>();
-//     private ArrayList<Integer> list = new ArrayList<Integer>();
-//     public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
-//         if(root == null) return listAll;
-//         list.add(root.val);
-//         target -= root.val;
-//         if(target == 0 && root.left == null && root.right == null)
-//             listAll.add(new ArrayList<Integer>(list));
-//         FindPath(root.left, target);
-//         FindPath(root.right, target);
-//         list.remove(list.size()-1);
-//         return listAll;
-//     }
-// }
