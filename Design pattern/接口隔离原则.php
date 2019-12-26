c59bebe3b52e71c10ed1860372e0158f8e1a484a
Mr.XYS <xys159753@gmail.com>
2019-12-27 03:33:13 +0800

    新增 接口隔离原则 示例代码

---
+++ Design pattern/接口隔离原则.php
@@ -0,0 +1,72 @@
+<?php
+interface Employee
+{
+    public function work(): void;
+
+    public function eat(): void;
+}
+
+class Human implements Employee
+{
+    public function work(): void
+    {
+        // ....working
+    }
+
+    public function eat(): void
+    {
+        // ...... eating in lunch break
+    }
+}
+
+class Robot implements Employee
+{
+    public function work(): void
+    {
+        //.... working much more
+    }
+
+    public function eat(): void
+    {
+        //.... robot can't eat, but it must implement this method
+    }
+}
+
+
+//================================
+
+interface Workable
+{
+    public function work(): void;
+}
+
+interface Feedable
+{
+    public function eat(): void;
+}
+
+interface Employee extends Feedable, Workable
+{
+}
+
+class Human implements Employee
+{
+    public function work(): void
+    {
+        // ....working
+    }
+
+    public function eat(): void
+    {
+        //.... eating in lunch break
+    }
+}
+
+// robot can only work
+class Robot implements Workable
+{
+    public function work(): void
+    {
+        // ....working
+    }
+}
