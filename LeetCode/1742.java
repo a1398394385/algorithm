5a360f36691271a83158b392e891327e40c362fa
XiaoYunSong <mr-xys@qq.com>
2020-12-31 11:30:20 +0800

    LeetCode每日刷题

---
+++ LeetCode/1742.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int numWays(int n) {
+        if (n == 0) return 1;
+        if (n < 4) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
