0c165ef81ea54646c602ed978aeb095f31ea6422
Mr.XYS <xys159753@gmail.com>
2021-03-07 17:57:27 +0800

    doc(LeetCode): 更新注释

--- LeetCode/84.java
+++ LeetCode/84.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 class Solution
 {
-    public static int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea(int[] heights) {
         int result = 0;
         for (int i = 0, len = heights.length; i < len; i++) {
             for (int j = i, min = Integer.MAX_VALUE; j < len; j++) {
@@ -13,7 +13,8 @@ class Solution
         return result;
     }
 
-    public static int largestRectangleArea1(int[] heights) {
+    // 单调递增栈
+    public int largestRectangleArea1(int[] heights) {
         int[] list = new int[heights.length + 1];
         System.arraycopy(heights, 0, list, 0, heights.length);
         list[heights.length] = 0;
@@ -21,8 +22,8 @@ class Solution
         int result = 0;
         for (int i = 0, len = list.length; i < len; i++) {
             while (!stack.isEmpty() && list[i] < list[stack.peek()]) {
-                int curHeight = stack.pop();
-                int acreage = list[curHeight] * (!stack.isEmpty() ? (i - stack.peek() - 1) : i);
+                int curIndex = stack.pop();
+                int acreage = list[curIndex] * (!stack.isEmpty() ? (i - stack.peek() - 1) : i);
                 result = Math.max(result, acreage);
             }
             stack.push(i);
@@ -38,22 +39,21 @@ class Solution
         left[0] = -1;
         right[n - 1] = n;
         int result = 0, temp;
+        // 左侧比当前柱子矮的第一根柱子的下标
         for (int i = 1; i < n; i++) {
             temp = i - 1;
             while (temp >= 0 && heights[temp] >= heights[i]) temp = left[temp];
             left[i] = temp;
         }
+        // 右侧比当前柱子矮的第一根柱子的下标
         for (int i = n - 2; i >= 0; i--) {
             temp = i + 1;
             while (temp < n && heights[temp] >= heights[i]) temp = right[temp];
             right[i] = temp;
         }
+        // right[i] - left[i] - 1 这个区间内所有柱子都比当前柱子高
         for (int i = 0; i < n; i++)
             result = Math.max(result, (right[i] - left[i] - 1) * heights[i]);
         return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Solution.largestRectangleArea1(new int[] {2, 1, 5, 6, 2, 3}));
     }
 }
