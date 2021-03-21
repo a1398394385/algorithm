a0195bcee7cc6fee2be80909e136a240f0b0225e
Mr.XYS <xys159753@gmail.com>
2021-03-22 00:29:21 +0800

    LeetCode每日刷题

--- LeetCode/191.java
+++ LeetCode/191.java
@@ -21,4 +21,24 @@ public class Solution
         }
         return count;
     }
+
+    public int hammingWeight2(int n) {
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
+        return n;
+    }
+
+    public int hammingWeight3(int n) {
+        int count = 0;
+        int i = 0;
+        while (i++ < 32) {
+            if ((n & 1) == 0)
+                count++;
+            n = n >> 1;
+        }
+        return 32 - count;
+    }
 }
